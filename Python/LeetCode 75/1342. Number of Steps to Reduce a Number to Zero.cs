337f2884bbddf52c67b72bf9744573696bd087b2
Zac Horton <45723831+ZacHorton@users.noreply.github.com>
2024-03-24 21:27:49 -0400

    Create 1342. Number of Steps to Reduce a Number to Zero.cs

---
+++ Python/LeetCode 75/1342. Number of Steps to Reduce a Number to Zero.cs
@@ -0,0 +1,16 @@
+public class Solution {
+  public int NumberOfSteps(int num) {
+    int numberOfSteps = 0;
+
+    while (num > 0) {
+      if (num % 2 == 0) {
+        num /= 2;
+      } else {
+        num -= 1;
+      }
+      numberOfSteps++;
+    }
+
+    return numberOfSteps;
+  }
+}
