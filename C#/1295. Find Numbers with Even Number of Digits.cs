4b4fb8acc5c5e8e1d5758895c71ee641c3a22a6b
Zac Horton <45723831+ZacHorton@users.noreply.github.com>
2024-03-26 10:27:48 -0400

    Create 1295. Find Numbers with Even Number of Digits.cs

---
+++ C#/1295. Find Numbers with Even Number of Digits.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int FindNumbers(int[] nums) {
+        int count = 0;
+        foreach (int num in nums) {
+            if (num.ToString().Length % 2 == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
